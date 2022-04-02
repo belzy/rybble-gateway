d85d6e3e2fd852e180f70b3eca9592d2c1f29433
belzy <brandon.g.elzy@gmail.com>
2022-04-01 19:14:53 -0500

    Setup tests

--- index.ts
+++ index.ts
@@ -1,2 +1,18 @@
+import express from 'express';
+import morgan from 'morgan';
 
-console.log("Hello World!");
+import apiRouter from './api/routes';
+
+const server = express();
+const PORT = process.env.PORT || 8080;
+
+const middleware = [
+  morgan('combined'),
+  apiRouter,
+];
+
+server.use(middleware);
+
+server.listen(PORT, () => {
+  console.log(`[server] Server listening on port ${ PORT }...`);
+});
